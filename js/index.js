5ba0b03180699320ae3c070d99b71ccb6c7475f2
Baly <baly818p@gmail.com>
2016-06-16 18:10:43 +0300

    5 commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var Homepage = {
+	init: function(){
+		this.mainSlider();
+		this.burgerMenu();
+	},
+	mainSlider: function(){
+		$('.main-slider .container').slick({
+
+		});
+				
+	},
+	burgerMenu: function(){
+		$('.burger-btn').on('click', function(){
+			if(!$(this).hasClass('active')){
+				$(this).addClass('active');
+				$(this).next().slideDown('fast');
+			} else {
+				$(this).removeClass('active');
+				$(this).next().slideUp('fast');
+			}
+		});
+	}
+};
+
+
+$(window).on('load', function(){
+	Homepage.init();
+});
+
+
+$('.button-down').on('click', function(){
+     $('html,body').animate({
+   scrollTop: $("#divToBeScrolledTo").offset().top
+});
+});
